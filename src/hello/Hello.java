32501e8c3723c3ce0fd710098833fce6cbac9a3c
g12928my <g12928my@s30435.tsuda.ac.jp>
2014-04-11 11:03:36 +0900

    新規にHelloWorld作成しました。

---
+++ src/hello/Hello.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author g12928my
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // TODO code application logic here
+    }
+}
